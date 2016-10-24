081fa3d9ec9f4d7a72a1cb2df0fb9fbdd3626909
bobwei <bob61016@gmail.com>
2016-10-24 16:32:21 +0800

    update packages and clean base cfg for linter

--- cfg/base.js
+++ cfg/base.js
@@ -1,23 +1,23 @@
-'use strict';
-let path = require('path');
-let defaultSettings = require('./defaults');
+/* eslint-disable global-require */
+const path = require('path');
+const defaultSettings = require('./defaults');
 
 // Additional npm or bower modules to include in builds
 // Add all foreign plugins you may need into this array
 // @example:
 // let npmBase = path.join(__dirname, '../node_modules');
 // let additionalPaths = [ path.join(npmBase, 'react-bootstrap') ];
-let additionalPaths = [];
+const additionalPaths = [];
 
 module.exports = {
-  additionalPaths: additionalPaths,
+  additionalPaths,
   port: defaultSettings.port,
   debug: true,
   devtool: 'eval',
   output: {
     path: path.join(__dirname, '/../dist/assets'),
     filename: 'app.js',
-    publicPath: defaultSettings.publicPath
+    publicPath: defaultSettings.publicPath,
   },
   devServer: {
     contentBase: './src/',
@@ -25,7 +25,7 @@ module.exports = {
     hot: true,
     port: defaultSettings.port,
     publicPath: defaultSettings.publicPath,
-    noInfo: false
+    noInfo: false,
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
@@ -35,13 +35,13 @@ module.exports = {
       sources: `${defaultSettings.srcPath}/sources/`,
       stores: `${defaultSettings.srcPath}/stores/`,
       styles: `${defaultSettings.srcPath}/styles/`,
-      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV
-    }
+      config: `${defaultSettings.srcPath}/config/${process.env.REACT_WEBPACK_ENV}`,
+    },
   },
   module: {},
-  postcss: function () {
+  postcss() {
     return [
-      require('autoprefixer')
+      require('autoprefixer'),
     ];
-  }
+  },
 };
