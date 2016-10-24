bb3d297ac99d36c1438af6bb4d020d5c062045f1
bobwei <bob61016@gmail.com>
2016-10-24 17:07:19 +0800

    fig config linter errors

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -4,9 +4,9 @@
  * Using [].push will add to the base array, so a require will alter
  * the base array output.
  */
-'use strict';
 
 const path = require('path');
+
 const srcPath = path.join(__dirname, '/../src');
 const dfltPort = 8000;
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -15,63 +15,55 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
  * Get the default modules object for webpack
  * @return {Object}
  */
-function getDefaultModules() {
+const getDefaultModules = () => {
   return {
     preLoaders: [
       {
         test: /\.(js|jsx)$/,
         include: srcPath,
-        loader: 'eslint-loader'
-      }
+        loader: 'eslint-loader',
+      },
     ],
     loaders: [
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax',
       },
       (() => {
         if (process.env.inlineCSSInJS) {
           return {
             test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader!postcss-loader?outputStyle=expanded'
+            loader: 'style-loader!css-loader!sass-loader!postcss-loader?outputStyle=expanded',
           };
         }
         return {
           test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader!postcss-loader?outputStyle=expanded')
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader!postcss-loader?outputStyle=expanded'),
         };
       })(),
       {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
-      {
-        test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      },
-      {
         test: /\.(png|jpg|gif|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
+        loader: 'url-loader?limit=8192',
       },
-      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    ],
   };
-}
+};
 
 module.exports = {
-  srcPath: srcPath,
+  srcPath,
   publicPath: '/assets/',
   port: dfltPort,
-  getDefaultModules: getDefaultModules
+  getDefaultModules,
 };
