c0bac9b458e5414a3f9ab1bed46f99cafa2fde86
bobwei <bob61016@gmail.com>
2016-11-18 00:53:43 +0800

    rm unused

--- server/middlewares/render.jsx
+++ server/middlewares/render.jsx
@@ -13,8 +13,6 @@ import HTML from '../../src/html';
 
 export default () => {
   const app = express();
-  app.set('views', path.join(__dirname, '../views'));
-  app.set('view engine', 'ejs');
 
   const routes = createRoutes(createMemoryHistory());
 
