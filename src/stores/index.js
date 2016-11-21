8fdc93086ca7d76861f9da2774d0aaa22c3c48d7
bobwei <bob61016@gmail.com>
2016-11-22 00:59:41 +0800

    add redux-persist

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,13 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import canUseDOM from 'fbjs/lib/ExecutionEnvironment';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 import rootReducer from '../reducers';
 
 export default (initialState) => {
   const middlewares = [thunkMiddleware];
-  return createStore(
+
+  const enhancers = [applyMiddleware(...middlewares)];
+  if (canUseDOM) {
+    enhancers.push(autoRehydrate());
+  }
+
+  const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(...middlewares),
+    compose(...enhancers),
   );
+
+  if (canUseDOM) {
+    persistStore(store, () => {
+      console.log('autoRehydrate completed');
+    });
+  }
+
+  return store;
 };
