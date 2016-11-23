366922a9db465475092d5b1e91edf056f5febfd7
bobwei <bob61016@gmail.com>
2016-11-23 11:59:52 +0800

    be able to logout

---
+++ src/modules/auth/selectors/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+export const userSelector = ({ user: { data } }) => ({
+  user: data,
+});
--- src/pages/Home/containers/index.jsx
+++ src/pages/Home/containers/index.jsx
@@ -1,18 +1,45 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import isEmpty from 'lodash.isempty';
 
 import styles from './index.scss';
+import { userSelector } from '../../../modules/auth/selectors';
+import * as actions from '../../../modules/auth/actions';
 
-const Home = () => (
+const Home = ({ user, logout }) => (
   <div className={styles.index}>
     <div className={styles.title}>
-      Hello World
+      React App Boilerplate
     </div>
-    <Link to="/login" className={`btn btn-default ${styles.btnLogin}`}>
-      Login
-    </Link>
+    {isEmpty(user) &&
+      <div>
+        <Link to="/login" className={`btn btn-default ${styles.btnLogin}`}>
+          Login
+        </Link>
+        <Link to="/admin" className={`btn btn-default ${styles.btnLogin}`}>
+          Admin Portal
+        </Link>
+      </div>
+    }
+    {!isEmpty(user) &&
+      <div>
+        <button className={`btn btn-default ${styles.btnLogin}`} onClick={logout}>
+          Logout
+        </button>
+      </div>
+    }
   </div>
 );
 
-export default connect()(Home);
+Home.propTypes = {
+  user: React.PropTypes.object,
+  logout: React.PropTypes.func,
+};
+
+export default connect(
+  userSelector,
+  dispatch => bindActionCreators(actions, dispatch),
+)(Home);
