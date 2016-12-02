ecae1c33bf2b25b6365fbfece31154faa4a88cca
bobwei <bob61016@gmail.com>
2016-12-03 00:57:33 +0800

    add handleSubmit to Filters

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, FormGroup, Col, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, propTypes } from 'redux-form';
 import compose from 'recompose/compose';
 import withProps from 'recompose/withProps';
 
 import FieldGroup from 'components/FieldGroup';
 
-const Filters = () => (
-  <Form horizontal>
+const Filters = ({ handleSubmit }) => (
+  <Form horizontal onSubmit={handleSubmit}>
     <Field
       name="name"
       component={withProps({ horizontal: true })(FieldGroup)}
@@ -20,8 +20,19 @@ const Filters = () => (
         autoFocus: true,
       }}
     />
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Button bsStyle="primary" type="submit">
+          Search
+        </Button>
+      </Col>
+    </FormGroup>
   </Form>
 );
+
+Filters.propTypes = {
+  ...propTypes,
+};
 
 export default compose(
   connect(),
