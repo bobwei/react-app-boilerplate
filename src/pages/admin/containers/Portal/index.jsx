6710abd84c88a8fc58ecf9c0b9537d94aca7db9a
bobwei <bob61016@gmail.com>
2016-12-05 19:45:27 +0800

    add visibleDataSelector

--- src/pages/admin/containers/Portal/index.jsx
+++ src/pages/admin/containers/Portal/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Row, Grid, Col, Panel } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { reduxForm, getFormValues } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import compose from 'recompose/compose';
 import withProps from 'recompose/withProps';
-// import { createSelector } from 'reselect';
-import R from 'ramda';
 
 import DataTable from 'components/DataTable';
 import Filters from 'components/Filters';
 
 import { columns as testColumns } from './model';
 import { focusSelector } from '../../helpers';
-import { dataSelector } from '../../selectors';
+import { visibleDataSelector } from '../../selectors';
 
 const FILTERS_FORM_NAME = 'filters';
 
@@ -21,21 +19,12 @@ const Portal = ({ columns }) => {
     connect(),
     reduxForm({
       form: FILTERS_FORM_NAME,
-      // eslint-disable-next-line no-unused-vars
-      onSubmit(submittedData) {},
+      onSubmit() {},
     }),
   )(Filters);
 
   const EnhancedDataTable = compose(
-    connect((state) => {
-      const filters = getFormValues(FILTERS_FORM_NAME)(state) || {};
-      const data = dataSelector(state).data;
-      const filteredData = R.filter(R.pipe(
-        R.prop('name'),
-        R.test(new RegExp(filters.q, 'i')),
-      ))(data);
-      return { data: filteredData };
-    }),
+    connect(visibleDataSelector(FILTERS_FORM_NAME)),
     withProps(() => ({
       columns,
     })),
--- src/pages/admin/selectors/index.jsx
+++ src/pages/admin/selectors/index.jsx
@@ -1,1 +1,14 @@
+import { getFormValues } from 'redux-form';
+import R from 'ramda';
+
 export const dataSelector = state => state.admin;
+
+export const visibleDataSelector = filterFormName => (state) => {
+  const { q } = getFormValues(filterFormName)(state) || {};
+  const { data } = dataSelector(state);
+  const filteredData = R.filter(R.pipe(
+    R.prop('name'),
+    R.test(new RegExp(q, 'i')),
+  ))(data);
+  return { data: filteredData };
+};
