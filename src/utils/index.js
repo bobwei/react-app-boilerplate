fa2793898c4eebbfd12f95444633ee67c069509e
bobwei <bob61016@gmail.com>
2017-01-07 01:33:16 +0800

    refactor envSelector with ramda

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-export const envSelector = (envs) => {
-  const {
-    CLIENT_HISTORY,
-    AUTH_API_BASE_URL, PARSE_SERVER_APPLICATION_ID, PARSE_SERVER_JAVASCRIPT_KEY,
-    PARSE_SERVER_API_BASE_URL,
-  } = envs;
-  return {
-    CLIENT_HISTORY,
-    AUTH_API_BASE_URL,
-    PARSE_SERVER_APPLICATION_ID,
-    PARSE_SERVER_JAVASCRIPT_KEY,
-    PARSE_SERVER_API_BASE_URL,
-  };
-};
+import R from 'ramda';
+
+export const envSelector = R.pick([
+  'CLIENT_HISTORY',
+  'AUTH_API_BASE_URL',
+  'PARSE_SERVER_APPLICATION_ID',
+  'PARSE_SERVER_JAVASCRIPT_KEY',
+  'PARSE_SERVER_API_BASE_URL',
+]);
