2c914a017c50490a17bc53811703944f398555ff
bobwei <bob61016@gmail.com>
2017-01-07 01:10:31 +0800

    rm auth prop from Home page

--- src/modules/pages/home/containers/Home/index.jsx
+++ src/modules/pages/home/containers/Home/index.jsx
@@ -1,76 +1,15 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
 import compose from 'recompose/compose';
-import withState from 'recompose/withState';
-import withProps from 'recompose/withProps';
-
-import { userSelector } from 'modules/auth/selectors';
-import * as actions from 'modules/auth/actions';
-import { isAuthenticated } from 'modules/auth/predicates';
-import Button from 'modules/ui/components/Button';
-import SubmitButton from 'modules/ui/components/SubmitButton';
 
 import styles from './index.scss';
 
-const Home = ({ user, logout, isLoggingOut }) => (
+const Home = () => (
   <div className={styles.index}>
     <div className={styles.title}>
       React App Boilerplate
     </div>
-    {!isAuthenticated(user) &&
-      <div>
-        <Button to="/admin" componentClass={Link} bsStyle="default" className={styles.btnLogin}>
-          Admin Portal
-        </Button>
-        <Button to="/login" componentClass={Link} bsStyle="default" className={styles.btnLogin}>
-          Login
-        </Button>
-        <Button to="/signup" componentClass={Link} bsStyle="default" className={styles.btnLogin}>
-          Signup
-        </Button>
-      </div>
-    }
-    {isAuthenticated(user) &&
-      <div>
-        <Button to="/admin" componentClass={Link} bsStyle="default" className={styles.btnLogin}>
-          Admin Portal
-        </Button>
-        <SubmitButton
-          className={styles.btnLogin}
-          submitting={isLoggingOut}
-          submitLabel="Logout"
-          bsStyle="default"
-          onClick={logout}
-        />
-      </div>
-    }
   </div>
 );
 
-Home.propTypes = {
-  user: React.PropTypes.object,
-  logout: React.PropTypes.func,
-  isLoggingOut: React.PropTypes.bool,
-};
-
-export default compose(
-  connect(
-    state => ({ user: userSelector()(state) }),
-    dispatch => bindActionCreators(actions, dispatch),
-  ),
-  withState('isLoggingOut', 'setIsLoggingOut', false),
-  withProps(({ logout, setIsLoggingOut }) => ({
-    logout() {
-      if (window.confirm('Are you sure you want to logout ?')) {
-        setIsLoggingOut(true);
-        return logout()
-          .then(() => setIsLoggingOut(false))
-          .catch(() => setIsLoggingOut(false));
-      }
-      return false;
-    },
-  })),
-)(Home);
+export default compose()(Home);
