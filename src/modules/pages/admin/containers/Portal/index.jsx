99e22e5fd2f4a785c149253e3764580267e4dc2d
bobwei <bob61016@gmail.com>
2017-01-07 21:45:37 +0800

    mv EnhancedDataTable out of Portal

--- src/modules/pages/admin/containers/Portal/index.jsx
+++ src/modules/pages/admin/containers/Portal/index.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { reduxForm } from 'redux-form';
 import compose from 'recompose/compose';
-import withProps from 'recompose/withProps';
 import lifecycle from 'recompose/lifecycle';
 import shallowEqual from 'recompose/shallowEqual';
 
@@ -15,7 +14,6 @@ import { transformQuery } from 'modules/parse-server/utils';
 
 import { columns as testColumns } from './model';
 import { focusSelector } from '../../helpers';
-import { dataAndFilterSelector } from '../../selectors';
 import Filters from '../../components/Filters';
 import styles from './index.scss';
 
@@ -26,57 +24,55 @@ const EnhancedFilters = compose(
   }),
 )(Filters);
 
-const Portal = ({ filterFormName, columns }) => {
-  const query = transformQuery(['username']);
-  const EnhancedDataTable = compose(
-    connect(
-      dataAndFilterSelector(filterFormName),
-      dispatch => bindActionCreators(actions, dispatch),
-    ),
-    lifecycle({
-      componentWillReceiveProps(nextProps) {
-        const { fetchData } = nextProps;
-        if (!shallowEqual(this.props.where, nextProps.where)) {
-          fetchData('_User', query(nextProps.where));
-        }
-      },
-      componentDidMount() {
-        const { fetchData, where } = this.props;
-        fetchData('_User', query(where));
-      },
-    }),
-    withProps(() => ({
-      settings: { keyField: 'objectId' },
-      columns,
-    })),
-  )(DataTable);
+const query = transformQuery(['username']);
 
-  return (
-    <Row>
-      <Grid>
-        <Col md={3} className={styles.columnLeft}>
-          <Panel header="Sidebar" className={styles.sidebar}>
-            <Nav bsStyle="pills" stacked>
-              <LinkContainer to="/admin">
-                <NavItem>Users</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Panel>
-        </Col>
-        <Col md={9}>
-          <Panel header="Filters" collapsible onEntered={focusSelector}>
-            <EnhancedFilters
-              form={filterFormName}
-            />
-          </Panel>
-          <Panel header={'Data'} bsStyle="info">
-            <EnhancedDataTable />
-          </Panel>
-        </Col>
-      </Grid>
-    </Row>
-  );
-};
+const EnhancedDataTable = compose(
+  connect(
+    state => ({ data: state.admin.data }),
+    dispatch => bindActionCreators(actions, dispatch),
+  ),
+  lifecycle({
+    componentWillReceiveProps(nextProps) {
+      const { fetchData } = nextProps;
+      if (!shallowEqual(this.props.where, nextProps.where)) {
+        fetchData('_User', query(nextProps.where));
+      }
+    },
+    componentDidMount() {
+      const { fetchData, where } = this.props;
+      fetchData('_User', query(where));
+    },
+  }),
+)(DataTable);
+
+const Portal = ({ filterFormName, columns }) => (
+  <Row>
+    <Grid>
+      <Col md={3} className={styles.columnLeft}>
+        <Panel header="Sidebar" className={styles.sidebar}>
+          <Nav bsStyle="pills" stacked>
+            <LinkContainer to="/admin">
+              <NavItem>Users</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Panel>
+      </Col>
+      <Col md={9}>
+        <Panel header="Filters" collapsible onEntered={focusSelector}>
+          <EnhancedFilters
+            form={filterFormName}
+          />
+        </Panel>
+        <Panel header={'Data'} bsStyle="info">
+          <EnhancedDataTable
+            settings={{ keyField: 'objectId' }}
+            columns={columns}
+          />
+        </Panel>
+      </Col>
+    </Grid>
+  </Row>
+);
 
 Portal.defaultProps = {
   filterFormName: 'filter',
