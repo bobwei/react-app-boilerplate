47f314260aea56df45a5f9319006c531d1e28375
bobwei <bob61016@gmail.com>
2017-01-07 14:51:26 +0800

    update modal style

--- src/modules/ui/components/Modal/index.jsx
+++ src/modules/ui/components/Modal/index.jsx
@@ -4,7 +4,7 @@ import { Modal as BootstrapModal } from 'react-bootstrap';
 import styles from './index.scss';
 
 const Modal = props => (
-  <BootstrapModal {...props}>
+  <BootstrapModal dialogClassName={styles.modalDialog} {...props}>
     <BootstrapModal.Body className={styles.modalBody}>
       {props.children}
     </BootstrapModal.Body>
