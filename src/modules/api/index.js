2522ad59a113d347ba682c22366444fd01eae8a3
bobwei <bob61016@gmail.com>
2017-07-25 15:24:05 +0800

    rm parse-server module

--- src/modules/api/index.js
+++ src/modules/api/index.js
@@ -1,8 +1,6 @@
 const configAPI = (__ENVS__) => {
   const AuthAPI = require('modules/auth/api').default;
   AuthAPI.config(__ENVS__);
-  const ParseServerAPI = require('modules/parse-server/api').default;
-  ParseServerAPI.config(__ENVS__);
 };
 
 export default configAPI;
--- src/modules/parse-server/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const transformQuery = fields => ({ q } = {}) => (!!q && {
-  where: JSON.stringify({
-    $or: fields.map(field => ({
-      [field]: { $regex: q },
-    })),
-  }),
-});
