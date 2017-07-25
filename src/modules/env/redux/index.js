226cbaa05207bb78ffc582be9d35ec1795ee69b9
bobwei <bob61016@gmail.com>
2017-07-25 14:45:21 +0800

    load env initialState from window.__ENVS__

--- src/modules/env/redux/index.js
+++ src/modules/env/redux/index.js
@@ -1,3 +1,6 @@
-export const initialState = {};
+/* eslint-disable no-underscore-dangle */
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
+
+const initialState = (canUseDOM) ? window.__ENVS__ : {};
 
 export default (state = initialState) => state;
