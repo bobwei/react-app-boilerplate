bee424a4df6f14ea1cccd0ac2af5b4bae09325c7
bobwei <bob61016@gmail.com>
2017-07-25 15:55:18 +0800

    rm syncHistoryWithStore

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -2,13 +2,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
 
 import theRoutes from '../../routes';
 
 const Root = ({ store, history, routes }) => (
   <Provider store={store}>
-    <Router history={syncHistoryWithStore(history, store)} routes={routes} />
+    <Router history={history} routes={routes} />
   </Provider>
 );
 
