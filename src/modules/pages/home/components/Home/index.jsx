b605d1ad70a47675674cf879aaa3c78cc17ffd07
bobwei <bob61016@gmail.com>
2017-07-26 00:38:24 +0800

    update pkg usage to reduce bundle size

--- src/modules/pages/home/components/Home/index.jsx
+++ src/modules/pages/home/components/Home/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Jumbotron, Grid } from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import Grid from 'react-bootstrap/lib/Grid';
 import compose from 'recompose/compose';
 
-const Home = () => (
+const Home = () =>
   <Grid>
     <Jumbotron>
       <h2>Hello, React App Boilerplate</h2>
-      <p>
-        This is a react app boilerplate with batteries included.
-      </p>
+      <p>This is a react app boilerplate with batteries included.</p>
     </Jumbotron>
-  </Grid>
-);
+  </Grid>;
 
 export default compose()(Home);
--- src/modules/pages/home/components/NavigationBar/index.jsx
+++ src/modules/pages/home/components/NavigationBar/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Link from 'react-router/lib/Link';
 import compose from 'recompose/compose';
 
-const NavigationBar = () => (
+const NavigationBar = () =>
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to="/">
-          React App Boilerplate
-        </Link>
+        <Link to="/">React App Boilerplate</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
-  </Navbar>
-);
+  </Navbar>;
 
 export default compose()(NavigationBar);
