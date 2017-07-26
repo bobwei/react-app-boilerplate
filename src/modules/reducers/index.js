bb92467cc84f74a44b2db456ec4b02bf190b0eb7
bobwei <bob61016@gmail.com>
2017-07-26 11:54:11 +0800

    rm react-router-redux

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { routerReducer } from 'react-router-redux';
 
 import env from 'modules/env/redux';
 
 export default combineReducers({
   form: formReducer,
-  routing: routerReducer,
   env,
 });
--- src/modules/stores/configureStore.js
+++ src/modules/stores/configureStore.js
@@ -2,15 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 import { persistStore, autoRehydrate } from 'redux-persist';
-import { routerMiddleware } from 'react-router-redux';
 
 import rootReducer from 'modules/reducers';
 
 export default (initialState, history, callback) => {
   const middlewares = [thunkMiddleware];
-  if (history) {
-    middlewares.push(routerMiddleware(history));
-  }
 
   const enhancers = [applyMiddleware(...middlewares)];
   if (canUseDOM) {
