1fcb1082856c83b94978e6d60b85e0a29151fa26
bobwei <bob61016@gmail.com>
2017-07-26 15:49:44 +0800

    update build script

--- scripts/build.jsx
+++ scripts/build.jsx
@@ -3,11 +3,11 @@ import 'dotenv/config';
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 
-import publicEnv from 'modules/env/selectors/publicEnv';
+import getPublicEnv from 'modules/env/selectors/getPublicEnv';
 
 import HTML from '../src/html';
 
-const env = publicEnv(process.env);
+const env = getPublicEnv(process.env);
 
 console.log(
   `<!doctype html>${renderToStaticMarkup(
