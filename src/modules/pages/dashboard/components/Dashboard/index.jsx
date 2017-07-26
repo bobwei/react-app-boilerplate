6528a356f2c09731fb803c4a41809550d7a74f1a
bobwei <bob61016@gmail.com>
2017-07-26 21:16:08 +0800

    update structure

--- src/modules/pages/dashboard/components/Dashboard/index.jsx
+++ src/modules/pages/dashboard/components/Dashboard/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
 
 const Dashboard = () =>
   <Grid>
-    <Jumbotron>
-      <h2>Dashboard</h2>
-    </Jumbotron>
+    <Row>
+      <Jumbotron>
+        <h2>Dashboard</h2>
+      </Jumbotron>
+    </Row>
   </Grid>;
 
 export default Dashboard;
--- src/modules/pages/home/components/Home/index.jsx
+++ src/modules/pages/home/components/Home/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
 
 const Home = () =>
   <Grid>
-    <Jumbotron>
-      <h2>Hello, React App Boilerplate</h2>
-      <p>This is a react app boilerplate with batteries included.</p>
-    </Jumbotron>
+    <Row>
+      <Jumbotron>
+        <h2>Hello, React App Boilerplate</h2>
+        <p>This is a react app boilerplate with batteries included.</p>
+      </Jumbotron>
+    </Row>
   </Grid>;
 
 export default Home;
