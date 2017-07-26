d58dd7e8f4853bc888790a6501e544d830d28ca1
bobwei <bob61016@gmail.com>
2017-07-27 00:39:42 +0800

    apply AuthenticationRedirect to Dashboard

--- src/modules/pages/dashboard/components/Dashboard/index.jsx
+++ src/modules/pages/dashboard/components/Dashboard/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
-import Redirect from 'react-router-dom/Redirect';
 import { requireAuth } from 'redux-modular-auth';
 import compose from 'recompose/compose';
+
+import AuthenticationRedirect from 'modules/auth/components/AuthenticationRedirect';
 
 const Dashboard = () =>
   <Grid>
@@ -17,6 +18,6 @@ const Dashboard = () =>
 
 export default compose(
   requireAuth({
-    UnauthenticatedComponent: () => <Redirect to="/login" />,
+    UnauthenticatedComponent: AuthenticationRedirect,
   }),
 )(Dashboard);
