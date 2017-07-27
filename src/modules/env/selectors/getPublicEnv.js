821de2528da0fbcedf5f86c8a59eadf51b60f306
bobwei <bob61016@gmail.com>
2017-07-27 22:47:13 +0800

    add parse-server module

--- src/modules/env/selectors/getPublicEnv.js
+++ src/modules/env/selectors/getPublicEnv.js
@@ -1,5 +1,11 @@
 import R from 'ramda';
 
-const getPublicEnv = R.pick(['BASE_SERVER_URL', 'BASE_API_URL']);
+const getPublicEnv = R.pick([
+  'BASE_SERVER_URL',
+  'BASE_API_URL',
+  'PARSE_SERVER_APPLICATION_ID',
+  'PARSE_SERVER_REST_API_KEY',
+  'PARSE_SERVER_URL',
+]);
 
 export default getPublicEnv;
