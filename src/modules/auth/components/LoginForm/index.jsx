e5be70f9136d9b3d9ee75e5837a03266380bfd18
bobwei <bob61016@gmail.com>
2017-07-27 16:56:13 +0800

    add login form

--- src/modules/auth/components/LoginForm/index.jsx
+++ src/modules/auth/components/LoginForm/index.jsx
@@ -1,32 +1,70 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
+import Form from 'react-bootstrap/lib/Form';
+import Alert from 'react-bootstrap/lib/Alert';
 import connect from 'react-redux/lib/connect/connect';
+import reduxForm from 'redux-form/lib/reduxForm';
+import Field from 'redux-form/lib/Field';
+import formPropTypes from 'redux-form/lib/propTypes';
 import compose from 'recompose/compose';
+
+import FieldGroup from 'modules/forms/components/FieldGroup';
 
 import mapStateToProps from './mapStateToProps';
 import mapDispatchToProps from './mapDispatchToProps';
 import redirectIfAuthenticated from '../../hocs/redirectIfAuthenticated';
 
-const LoginForm = ({ login }) =>
+const LoginForm = ({ handleSubmit, submitting, pristine, error }) =>
   <Grid>
     <Row>
       <Col md={4} mdOffset={4}>
-        <Button onClick={login} block>
-          Login
-        </Button>
+        <Form onSubmit={handleSubmit}>
+          {!pristine &&
+            error &&
+            <Alert bsStyle="danger">
+              <p>
+                {error}
+              </p>
+            </Alert>}
+          <Field
+            name="username"
+            component={FieldGroup}
+            label="Username"
+            formControlProps={{
+              type: 'text',
+              placeholder: 'Username',
+              autoFocus: true,
+            }}
+          />
+          <Field
+            name="password"
+            component={FieldGroup}
+            label="Password"
+            formControlProps={{
+              type: 'password',
+              placeholder: 'Password',
+            }}
+          />
+          <Button type="submit" disabled={submitting} block>
+            {submitting ? 'Loading...' : 'Login'}
+          </Button>
+        </Form>
       </Col>
     </Row>
   </Grid>;
 
 LoginForm.propTypes = {
-  login: PropTypes.func.isRequired,
+  ...formPropTypes,
 };
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   redirectIfAuthenticated(),
+  reduxForm({
+    form: 'login',
+    onChange: (values, dispatch, { clearSubmitErrors }) => clearSubmitErrors(),
+  }),
 )(LoginForm);
--- src/modules/auth/components/LoginForm/mapDispatchToProps.js
+++ src/modules/auth/components/LoginForm/mapDispatchToProps.js
@@ -1,8 +1,22 @@
 import { login } from 'redux-modular-auth';
+import SubmissionError from 'redux-form/lib/SubmissionError';
+import R from 'ramda';
 
 const mapDispatchToProps = dispatch => ({
-  login: () =>
-    dispatch(login({ objectId: 'objectId', sessionToken: 'sessionToken' })),
+  onSubmit: R.ifElse(
+    R.equals({ username: 'username', password: 'password' }),
+    () => {
+      R.compose(dispatch, login)({
+        objectId: 'objectId',
+        sessionToken: 'sessionToken',
+      });
+    },
+    () => {
+      throw new SubmissionError({
+        _error: `Should be "username" and "password"`,
+      });
+    },
+  ),
 });
 
 export default mapDispatchToProps;
