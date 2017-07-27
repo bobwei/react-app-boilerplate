55dfd9549854bfd4b8ddf86360d1059b1ebbda4a
bobwei <bob61016@gmail.com>
2017-07-27 21:02:55 +0800

    use createRequest as dependency injection

--- src/modules/requests/withRequest.js
+++ src/modules/requests/withRequest.js
@@ -3,14 +3,17 @@ import { createSelector } from 'reselect';
 import R from 'ramda';
 import compose from 'recompose/compose';
 
-import createRequest from './createRequest';
-
 /*
   1. Select env from redux store
   2. Use createRequest factory function to create request with env
   3. Inject request as requestPropName
 */
-const withRequest = ({ requestPropName = 'apiRequest' } = {}) =>
+const withRequest = (
+  {
+    requestPropName = 'apiRequest',
+    createRequest = require('./createRequest').default,
+  } = {},
+) =>
   compose(
     connect(
       createSelector(
