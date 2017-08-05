1a27d4e31d07d32bc79098c52916fcdd4d2adbcb
Bob Wei <bob61016@gmail.com>
2017-08-05 22:58:52 +0800

    Add withLoadingEffect

---
+++ src/modules/utils/hocs/withLoadingEffect.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension, react/prop-types */
+import React from 'react';
+import compose from 'recompose/compose';
+
+const withLoadingEffect = () =>
+  compose(WrappedComponent => props =>
+    <div style={{ opacity: props.isLoading ? 0.6 : 1 }}>
+      <WrappedComponent {...props} />
+    </div>,
+  );
+
+export default withLoadingEffect;
