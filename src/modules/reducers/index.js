5490f379140ab0407c36b0eab00ff2ac4794ddb8
Bob Wei <bob61016@gmail.com>
2017-08-11 16:04:04 +0800

    Add redux-modular-alerts

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { createReducer as createAuthReducer } from 'redux-modular-auth';
 import { createReducer as createModelReducer } from 'redux-modular-models';
+import { createReducer as createAlertsReducer } from 'redux-modular-alerts';
 
 import env from 'modules/env/redux';
 
@@ -12,4 +13,5 @@ export default combineReducers({
   models: createModelReducer({
     models: [],
   }),
+  alerts: createAlertsReducer(),
 });
